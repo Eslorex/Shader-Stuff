0b2d04ff52342e2d3cdde96f4d8ed5ec98ef5898
Eslorex <68527335+Eslorex@users.noreply.github.com>
2024-11-22 18:35:14 +0300

    Add files via upload

---
+++ Shadertoy/Noise-Shader.glsl
@@ -0,0 +1,49 @@
+// Simplex noise function (2D)
+vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
+float snoise2D(vec2 v)
+{
+    const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                        0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                        -0.577350269189626, // -1.0 + 2.0 * C.x
+                        0.024390243902439); // 1.0 / 41.0
+    vec2 i = floor(v + dot(v, C.yy) );
+    vec2 x0 = v -   i + dot(i, C.xx);
+    vec2 i1;
+    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+    vec4 x12 = x0.xyxy + C.xxzz;
+    x12.xy -= i1;
+    i = mod289(i); // Avoid truncation
+    vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+                      + i.x + vec3(0.0, i1.x, 1.0 ));
+    vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), 
+                            dot(x12.zw, x12.zw)), 0.0);
+    m = m * m;
+    m = m * m;
+    vec3 x = 2.0 * fract(p * C.www) - 1.0;
+    vec3 h = abs(x) - 0.5;
+    vec3 ox = floor(x + 0.5);
+    vec3 a0 = x - ox;
+    m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
+    vec3 g;
+    g.x = a0.x * x0.x + h.x * x0.y;
+    g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+    return 130.0 * dot(m, g);
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = fragCoord.xy / iResolution.xy;
+    
+    uv = uv * 3.0 - 1.0;
+    uv.x *= iResolution.x / iResolution.y; 
+    float timeFactor = sin(iTime * 0.5) * 0.5 + 1.0; // OSC
+    vec2 animatedUV = uv + vec2(cos(iTime + uv.y * 3.0), sin(iTime + uv.x * 3.0)) * 0.2 * timeFactor;
+    
+    float o = sin(snoise2D(animatedUV * 4.0) * 4.0 + iTime); // FQ
+    
+    float pattern = sin(o * 10.0 + iTime * 2.0) * 0.5 + 0.5;
+    
+    fragColor = vec4(vec3(pattern), 1.0);
+}
